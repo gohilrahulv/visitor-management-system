bc0ebfde6d92bbd14639b7813800c0a7c3c512f6
gohilrahulv <gohilrahul@gmail.com>
2025-09-16 08:22:03 +0530

    fixes

---
+++ back/vms-backend/src/main/java/com/vms/VmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsBackendApplication.class, args);
+	}
+
+}
